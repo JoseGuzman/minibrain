2883f90f2693f3aa9564dfa293aaca4781087a43
JoseGuzman <sjm.guzman@gmail.com>
2019-10-07 18:56:30 +0200

    pluging to remove single spikes

--- phy/myClusterView.py
+++ phy/myClusterView.py
@@ -14,6 +14,7 @@ c.TemplateGUI.plugins = ['ISImedian']
 import numpy as np
 
 from phy import IPlugin, connect
+from phy import emit
 from phy.cluster import Clustering
 
 
@@ -34,7 +35,8 @@ class Test(IPlugin):
             def on_cluster(sender, event):
                 """
                 Called every time a cluster assignment or cluster 
-                group/label changes.
+                group/label changes. It will print a message
+                on the console where phy starts.
                 """
                 print("Clusters update: %s" % event)
 
@@ -46,8 +48,8 @@ class ISImedian(IPlugin):
     def attach_to_controller(self, controller):
         """
         This function is called at initialization time before
-        creation of the supervisor object (s) which controls the 
-        clusters and the similarity view.
+        creation of the supervisor object (s) which controls 
+        ClusterView and SimilarityView.
         """
         
         def medianisi(cluster_id):
@@ -58,6 +60,69 @@ class ISImedian(IPlugin):
             t = controller.get_spike_times(cluster_id).data
             return np.median(np.diff(t)) if len(t)>2 else 0
 
+        def cc_index(cluster_id):
+            """
+            computes the percentage of spikes within the
+            refractory period.
+            """
+            pass
+
         # will appear in ClusterView and SimilarityView
         controller.cluster_metrics['ISI.median'] = \
             controller.context.memcache(medianisi)
+
+class SpikeSplitter(IPlugin):
+    """
+    This Pluging select single spikes to be splitted 
+    """
+    def attach_to_controller(self, controller):
+        """
+        This function is called at initialization time before
+        creation of the supervisor object (s) which controls  
+        ClusterView and SimilarityView.
+        """
+        @connect(sender=controller)
+        def on_gui_ready(sender, gui):
+            """
+            Called when the GUI and all objects are fully loaded.
+            It makes sure that controller.supervisor is properly defined.
+            """
+            #------------------------------------------
+            # File->Display message
+            #------------------------------------------
+            gui.file_actions.separator()
+            @gui.file_actions.add()
+            def show_selected_clusters():
+                """
+                Display selected clusters in the status bar
+                """
+                sel = controller.supervisor.selected
+                gui.status_message = "Selected cluster/s: %s"%sel
+            
+            #------------------------------------------
+            # in Select->MyPlugins:
+            #------------------------------------------
+            gui.select_actions.separator()
+            myparams = dict(submenu='MyPlugins', shortcut = 'alt+p',
+                prompt=True, prompt_default=lambda: 0)
+            @gui.select_actions.add( **myparams )
+            def split_spike(time):
+                """
+                split the spike next to the time entered
+                """
+                #@controller.supervisor.cluster_view.get_ids
+                pspike = int(time * 30000)
+                # get id from selected cluster
+                mycluster_id = controller.supervisor.selected
+                # get the sample of the first spike
+                t = controller.get_spike_times(mycluster_id[0])
+                p = controller.get_spike_ids(mycluster_id[0])
+                x = np.argmax(t>time) 
+                # remove this p[x]
+                #emit('action', s.action_creator, 'split', list(p[x]))
+                gui.status_message = 'Spike id: %s'%p[x]
+
+        
+
+            
+
