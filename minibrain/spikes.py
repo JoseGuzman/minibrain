1909964b1558c8620d2f648573d53be7e97af285
JoseGuzman <sjm.guzman@gmail.com>
2019-10-31 10:46:24 +0100

    units object allow selecting spikes in pulse

--- minibrain/spikes.py
+++ minibrain/spikes.py
@@ -14,6 +14,7 @@ Example:
 >>> myrec = DataLoader('./') # will load shankA, shankB, shankC and shankD
 """
 
+import copy 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -40,16 +41,17 @@ class Units(object):
             the shank of the silicon probe to read 
         """
     
-        df_list = list() # a list with dfs for different shanks
-        dict_unit = dict() # a dictionary with all units from different shanks 
+        df_list = list() # list of dataframe
+        dict_unit = dict() # a dictionary with all units
         for myshank in shank:
             mypath = path + '/shank' + myshank + '/continuous.GUI/'
             myfile = mypath + 'cluster_info.tsv'
             df = pd.read_csv(myfile, sep = '\t')
             df['shank'] = df['shank'].apply(read_shank) # map probes
-        
-            # choose good units
+            
+            # choose only good units
             df_unit = df[ (df['group']=='good') ]
+            df_unit = df_unit.drop('group', 1) # 1 is for dropping column
         
             # read good units
             spike_times = np.load(mypath + 'spike_times.npy')
@@ -57,12 +59,48 @@ class Units(object):
         
             for i in df_unit['id'].values:
                 mykey = str(i) + myshank
-                dict_unit[ mykey] =  spike_times[ np.where(spike_clusters==i) ] 
-            
-            df_unit['id'] = df_unit['id'].apply(lambda x: str(x) + myshank) # change to have unique identifiers
-            df_list.append(df_unit)
+                dict_unit[mykey] = spike_times[np.where(spike_clusters==i)] 
+
+            # change to have unique identifiers
+            df_unit['id'] = df_unit['id'].apply(lambda x: str(x) + myshank)             df_list.append(df_unit)
         
         self.df = pd.concat(df_list).reset_index(drop=True)
-        self.id = dict_unit
+        self.unit = dict_unit
         print('%d units found'%len(self.df))
-    
+
+    def pulsecopy(self, pulse):
+        """
+        Returns a new instance of the Unit class, 
+        but only with the spikes entered in pulse.
+
+        Arguments
+        ---------
+        pulse (list)
+            a list of two values start and end values
+        """
+        myclass = self.__class__
+        myunit = myclass.__new__(myclass)
+
+        # make deep copies
+        mydict = copy.deepcopy( self.unit )
+        mydf = copy.deepcopy(self.df)
+        
+        for key, values  in mydict.items():
+            mytimes = list()
+            for p in pulse:
+                start, end = p
+                spk_times = np.array(values)
+                x = np.logical_and(spk_times>start, spk_times<end)
+                if not spk_times[x].size == 0:
+                    newval = spk_times[x].tolist()
+                    mytimes.extend( newval )
+            mydict[key] = np.array(mytimes)
+            
+            index = mydf.index[ mydf['id']==key]
+            mydf.loc[index, 'n_spikes'] =  int(len(mytimes))
+
+        # set attributes of new object 
+        setattr(myunit, 'unit', copy.deepcopy(mydict))
+        setattr(myunit, 'df', mydf)
+
+        return myunit
