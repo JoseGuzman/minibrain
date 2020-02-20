d66315b2be51a9e6f93648e6742163efd2b0b7b6
JoseGuzman <sjm.guzman@gmail.com>
2020-02-20 17:48:55 +0100

    implementation and unittesting for Fourier analysis

--- test/power_unittest.py
+++ test/power_unittest.py
@@ -12,6 +12,7 @@ import sys
 import numpy as np
 from numpy import pi as PI
 from minibrain.lfp import power
+from minibrain.lfp import fourier_spectrum
 
 
 class TestPowerObject(unittest.TestCase):
@@ -23,45 +24,81 @@ class TestPowerObject(unittest.TestCase):
         """
         Create a sine wave
         """
-        self.log = logging.getLogger('Log info ')
+        self.logW = logging.getLogger("Welch's method")
+        self.logF = logging.getLogger("Discrete Fourier")
         self.srate = 30000 # 30 kHz
 
         # 60-seg sine wave of 10 units amplitude
         t = np.linspace(0, 60, num = 60*self.srate)
         self.wave = lambda f: np.sin( 2*PI*f*t )
 
+    def test_Fourier_amplitudes(self):
+        """
+        Test that the discrete Fourier transform computes
+        the right amplitudes.
+        """
+        self.logF.debug('Amplitude analysis with Fourier\n')
+        for amp in np.random.uniform(low=1,high=100,size = 10):
+            mywave = amp*self.wave(5) # 5 Hz wave 
+            f, myamp = fourier_spectrum(data = mywave, srate=self.srate)
+
+            info = 'Wave amplitude = {:7.5f}, Fourier = {:7.5f}'
+            self.logF.debug(info.format(amp, myamp.max()))
+
+            # Once rounded to 4 places, the difference is 0.00001
+            self.assertAlmostEqual(amp, myamp.max(), 4, 1e-5)
+        
+    def test_Fourier_frequencies(self):
+        """
+        Test that the discrete Fourier transform computes
+        the right frequencies.
+        """
+        self.logF.debug('Frequency analysis with Fourier\n')
+        for freq in np.random.randint(low=1, high=100, size = 10):
+            mywave = self.wave(freq)
+            f, myamp = fourier_spectrum(data = mywave, srate=self.srate)
+            myfreq = f[myamp.argmax()]
+
+            info = 'Wave frequency = {:7.5f}, Fourier = {:7.5f}'
+            self.logF.debug( info.format(freq, myfreq) )
+
+            # Once rounded to 4 places, the difference is 0.00001
+            self.assertAlmostEqual(freq, myfreq, 4, 1e-5)
+        
     def test_Welch_amplitudes(self):
         """
-        Test that the Welch's periodogram in power compute 
+        Test that the Welch's periodogram in power computes
         the right frequencies.
         """
         myparams = dict(srate = self.srate, segment = self.srate*5)
 
-        self.log.debug('\nAmplitude analysis\n')
+        self.logW.debug('Amplitude analysis with Welch\n')
         for amp in np.random.uniform(low=1,high=100,size = 10):
             mywave = amp*self.wave(5) # 5 Hz wave 
             f, mypower = power.welch(data = mywave, **myparams)
             myamp = np.sqrt( mypower.max() )
-            info = 'Wave amplitude = {:7.5f}, Welch = {:7.5f}'.format(amp, myamp)
-            self.log.debug(info)
+
+            info = 'Wave amplitude = {:7.5f}, Welch = {:7.5f}'
+            self.logW.debug(info.format(amp, myamp))
 
             # Once rounded to 5 places, the difference is 0.00001
             self.assertAlmostEqual(amp, myamp, 5, 1e-5)
 
     def test_Welch_frequencies(self):
         """
-        Test that the Welch's periodogram in power compute 
+        Test that the Welch's periodogram in power computes
         the right frequencies.
         """
         myparams = dict(srate = self.srate, segment = self.srate*5)
 
-        self.log.debug('\nFrequency analysis\n')
+        self.logW.debug('Frequency analysis with Welch\n')
         for freq in np.random.randint(low=1, high=100, size = 10):
             mywave = self.wave(freq)
             f, mypower = power.welch(data = mywave, **myparams)
             myfreq = f[mypower.argmax()]
-            info = 'Wave frequency = {:7.5f}, Welch = {:7.5f}'.format(freq,myfreq)
-            self.log.debug(info)
+
+            info = 'Wave frequency = {:7.5f}, Welch = {:7.5f}'
+            self.logW.debug( info.format(freq,myfreq) )
 
             # Once rounded to 5 places, the difference is 0.00001
             self.assertAlmostEqual(freq, myfreq, 5, 1e-5)
