ffdc7ce0ae889cb3922ec9907b99a8ab12093c06
JoseGuzman <sjm.guzman@gmail.com>
2020-05-08 09:52:41 +0200

    testpulse logging

--- PulsePal/testpulse.py
+++ PulsePal/testpulse.py
@@ -7,8 +7,10 @@ Jose Guzman, jose.guzman<at>guzman-lab.com
 This script delivers one PULSE of 500 ms
 every x seconds repeteadly upon activation of Trig1.
 
-To load a 500 ms Pulse delivered every 5 sec in Out2 type:
->>> python testpulse.py -s 5 -out 2
+To load a 500 ms Pulse repeteadly delivered. For example,
+>>> python testpulse.py -s 5 -out 2 -dir '/data/
+
+will deliver a pulse in Out2 every 5 seconds and save a log file in /data
 
 Activate Trigger1 to activate/deactivate the repeated pulse
 """
@@ -45,19 +47,33 @@ def loadpulse(args):
 if __name__ == '__main__':
     import argparse
     import logging
+    from datetime import datetime
 
+    # Parser
     parser = argparse.ArgumentParser(description='repeats a 500 ms pulse')
-    # --out set the Output channel
+    # --out output channel
     parser.add_argument('--out', '-o', type = int, 
         help = 'Output channel', required = True)
-    # --sec set the interpulse interval
+    # --sec repetition interval 
     parser.add_argument('--sec', '-s', type = int, 
         help = 'repetition interval', required = True)
+    # --dir  directory to save log (default /data)
+    parser.add_argument('--dir', '-d', type = str, default = '.',
+        help = 'directory to save log (default current)', required = False)
     myargs = vars(parser.parse_args())
 
-    logging.basicConfig(filename = 'testpulse.log', level = logging.DEBUG)
-    logging.info('Loading testpulse.py in PulsePal v2.0')
+    # Logging
+    mytime = datetime.now()
+    lognow = mytime.strftime('%Y-%m-%d_%H-%M-%S')
+    logdate = mytime.strftime('%Y-%m-%d')
+    logtime = mytime.strftime('%H-%M-%S')
+    myfname = myargs['dir'] + '/testpulse_' + lognow + '.log'
+    logging.basicConfig(filename = myfname, level = logging.DEBUG)
+    logging.info(' Loading testpulse.py in PulsePal v2.0')
+    logging.info(' Date: %s'%logdate)
+    logging.info(' Time: %s'%logtime)
+    
     output = (myargs['out'], myargs['sec'])
-    logging.info('The pulse was delivered in OUT[%d] every %d sec'%(output))
+    logging.info(' OUT[%d] delivered 500 ms every %d sec'%(output))
 
     loadpulse(args = myargs)
