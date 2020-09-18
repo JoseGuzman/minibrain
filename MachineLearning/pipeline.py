3e6c41df1e551982a86fa02d24f6b555635c5dac
JoseGuzman <sjm.guzman@gmail.com>
2020-09-18 15:08:57 +0200

    add colors

--- MachineLearning/pipeline.py
+++ MachineLearning/pipeline.py
@@ -4,9 +4,21 @@ pipeline.py
 Jose Guzman, jose.guzman <at> guzman-lab.com
 
 Created: Thu Sep 17 21:56:11 CEST 2020
+
+Contains transformers
 """
 
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+
+
+mycolors = {'TSCp5_30s':     '#FFA500', # orange
+          'TSCp5_32s':       '#4169E1', # royalblue
+          'DLX_bluered':     '#00BFFF', # deepskyblue
+          'DLX_Cheriff':     '#006400', # darkgreen
+          'DLX_Cheriff_AS' : '#4B0082'  # indigo
+    }
+
 
 class ColumnsDelete():
     """
@@ -34,5 +46,8 @@ class ColumnsDelete():
         return mydf
 
 
+class OneHotEncoder():
 
+
+Pipeline( steps = )
 
