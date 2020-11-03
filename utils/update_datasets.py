4a07bfd6846964a0aec6ae6cf25a51ca33cebbd8
JoseGuzman <sjm.guzman@gmail.com>
2020-11-03 12:12:18 +0100

    efficient script to update DataSets

--- utils/update_datasets.py
+++ utils/update_datasets.py
@@ -16,76 +16,65 @@ In addition, it will update the minibrain repository
 
 """
 import logging
-import sys
-import glob
 import pandas as pd
 
-def read_waveform(path):
-    """
-    Creates csv of normalized waveforms (*waveforms.csv) and kinetics
-    of the spikes (*spikes.csv) in the directory entered in path.
-
-    Returns
-    -------
-    A dataFrame with normalized waveforms and spike kinetics for 
-    the experiments entered in path (they correspond to a different
-    organoid type).
-
-    """
-    logging.basicConfig(stream = sys.stdout, level = logging.INFO)
-    
-    files = glob.glob(path + 'waveforms/*df')
-    frames = [pd.read_pickle(wave) for wave in files ]
-    dfspikes = pd.concat( frames, sort = False, ignore_index = True)
-    # set uid as pandas index
-    dfspikes.set_index(dfspikes.uid, inplace=True)
-    dfspikes.drop('uid', axis = 1, inplace = True)
-
-    # we order by index to have the latest recording on top
-    dfspikes.sort_index(ascending = False, inplace = True) 
-
-    logging.info(f'Reading {dfspikes.shape[0]:4d} spike waveforms in {path}')
-
-    OrgID = dfspikes.index[0][:2] # first two alphanumeric values
-
-    # create local *spikes.csv include index to have uid !!!!
-    myname = path + OrgID + "_spikes.csv"
-    dfspikes.loc[:, dfspikes.columns !='waveform'].to_csv(myname,index=True)
-    
-    # create local *waveforms.csv include index to have uid !!!
-    dfwaveforms = pd.DataFrame(dfspikes['waveform'].tolist(), 
-            index = dfspikes.index)
-    myname = path + OrgID + "_waveforms.csv"
-    dfwaveforms.to_csv(myname, index = True)
-
-    return (dfspikes) # uid is now included as index
-
 if __name__ == '__main__':
-    import os 
-    HOME = os.getenv('HOME')
-    # directories where recordings are saved
-    VTpath = HOME + '/Guzman.VCB/DLXi56/Analysis/SiliconProbes/VT/'
-    FSpath = HOME + '/Guzman.VCB/DLXi56/Analysis/SiliconProbes/FS/'
-    TCpath = HOME + '/Guzman.VCB/TSC2/Analysis/SiliconProbes/'
+    from pathlib import Path
+    from minibrain.transformers import PandasReader
 
-    dfVT = read_waveform(VTpath)
-    dfTC = read_waveform(TCpath)
-    dfFS = read_waveform(FSpath)
+    # Globals
+    HOME = Path.home()
+    GIT = Path(HOME, 'git', 'minibrain', 'DataSets')
+    # directories where recordings are located
+    VT = Path(HOME, 'Guzman.VCB', 'DLXi56', 'Analysis' , 'SiliconProbes',
+        'VT', 'waveforms')
+    FS = Path(HOME, 'Guzman.VCB', 'DLXi56', 'Analysis' , 'SiliconProbes',
+        'FS', 'waveforms')
+    FH = Path(HOME, 'Guzman.VCB', 'DLXi56', 'Analysis' , 'SiliconProbes',
+        'FH', 'waveforms')
+    TC = Path(HOME, 'Guzman.VCB', 'TSC2', 'Analysis' , 'SiliconProbes',
+        'waveforms')
+    AP = Path(HOME, 'Guzman.VCB', 'AP', 'Analysis' , 'SiliconProbes',
+        'waveforms')
+
+    pathlist = [VT, FS, FH, TC, AP]
+
+
+    dfreader = PandasReader(extension = '*df') # all *df files
+
+    dfspikes_list = list()
+    dfwaveforms_list = list()
+    dforganoid_list = list()
+    nsamples = 0 # counter
+    for mypath in pathlist:
+        dfspikes = dfreader.transform(path = mypath)
+        print(f'{"#"*73}')
+        print(f'{mypath}')
+        print(f'{dfreader.nrecords:3d} spikes')
+        print(f'{len(dfreader):3d} samples\n')
+        nsamples +=len(dfreader)
+        dfwaveforms = pd.DataFrame(dfspikes['waveform'].tolist(),
+                index = dfspikes.index)
+        dfspikes.drop('waveform', axis = 1, inplace = True)
+        dforganoid = pd.DataFrame(dfspikes['organoid'].tolist(),
+                index = dfspikes.index)
+
+        dfspikes_list.append(dfspikes)
+        dfwaveforms_list.append(dfwaveforms)
+        dforganoid_list.append(dforganoid)
 
     # merge in GitHub all organoids together 
-    # verify that we have unique index
-    dfspikes = pd.concat([dfVT, dfTC, dfFS], ignore_index = False)
+    # verify that we have unique index with ignore_index = False
+    # sort=True is because AP organoids have location column
+    dfspikes = pd.concat(dfspikes_list, ignore_index = False, sort=True)
+    dfwaveforms = pd.concat(dfwaveforms_list, ignore_index = False)
+    dforganoid = pd.concat(dforganoid_list, ignore_index = False)
 
-    git = HOME + '/git/minibrain/DataSets/'
 
-    # Create/update spikes.csv
-    logging.info(f'Saving  {dfspikes.shape[0]:4d} spike waveforms in {git}')
-    name = git + 'spikes.csv'
-    dfspikes.loc[:,dfspikes.columns !='waveform'].to_csv(name, index=True)
+    # Create/update Datasets in git 
+    #logging.info(f'Saving  {dfspikes.shape[0]:4d} spike waveforms in {git}')
+    dfspikes.to_csv(Path(GIT ,'spikes.csv'), index = True)
+    dfwaveforms.to_csv(Path(GIT, 'waveforms.csv'), index = True)
+    dforganoid.to_csv(Path(GIT, 'organoID.csv'), index = True)
+    print(f'Total: {dfspikes.shape[0]:3d} spikes in {nsamples} samples')
 
-    # Create/update waveforms.csv
-    name = git + 'waveforms.csv'
-    dfwaveforms = pd.DataFrame(dfspikes['waveform'].tolist(), 
-            index = dfspikes.index)
-    dfwaveforms['organoid'] = dfspikes['organoid']
-    dfwaveforms.to_csv(name, index=True)
