b13b600397d54169a3e18298fa498f698d78e436
JoseGuzman <sjm.guzman@gmail.com>
2021-01-19 17:13:43 +0100

    update colors

--- minibrain/utils.py
+++ minibrain/utils.py
@@ -8,8 +8,8 @@ Created: Fri Nov 13 09:53:04 CET 2020
 Various useful objects
 """
 # custom matplotlib colors for different organoids
-mycolors = {'TSCp5_30s':     '#FFA500', # orange
-          'TSCp5_32s':       '#4169E1', # royalblue
+mycolors = {'TSCp5_30s':     '#4169E1', # tab:blue
+          'TSCp5_32s':       '#d62728', # tab:red
           'DLX_bluered':     '#32CD32', # limegreen 
           'DLX_Cheriff':     '#228B22', # forestgreen
 #          'DLX_Cheriff_AS':  '#32CD32', # limegreen
