e19084cdd719a30f1f016ee7578b1efe33a17496
JoseGuzman <sjm.guzman@gmail.com>
2021-03-17 14:12:43 +0100

    configuration file to create E probes

--- Matlab/createE_Probes.m
+++ Matlab/createE_Probes.m
@@ -1,22 +1,22 @@
 %=========================================================================
-% createP_Probes.m
+% createLinear_Probes.m
 %
-% Jose Guzman, jose.guzman<at>guzman-lab.com
-% 
-% Created: Wed 17 Mar 2021 12:24:39 PM CET
+% Created: Thu Nov 12 11:11:47 CET 2020
 %
-% Creates the coordinates of the ASSY77_P probes of Cambridge Neurotech 
+% Creates the coordinates of ASSY-77-E1 and E2 CambridgeNeurotech probes
 % It will design it as a linear probe (one probe after each other)
 % because both KiloSort and Phy are designed better for linear probes.
 % it will not affect the sorting, since the probes are independent.
 %=======================================================================
 
-
 %%=======================================================================
-%% 64_P.mat
+%% 64E.mat
+%% 4 shanks names A, B, C and D.
+%% shanks contain 16 electrodes
+%% three auxiliary channels
 %%=======================================================================
-fname2 = '~/git/minibrain/Matlab/Kilosort2/configFiles/64P.mat';
-fname3 = '~/git/minibrain/Matlab/Kilosort3/configFiles/64P.mat';
+fname2 = '~/git/minibrain/Matlab/Kilosort2/configFiles/64E.mat';
+fname3 = '~/git/minibrain/Matlab/Kilosort3/configFiles/64E.mat';
 
 Nchannels = 67; % Total number of channels
 connected = true(Nchannels,1); % zero if bad channel 
@@ -25,25 +25,22 @@ chanMap = 1:Nchannels;
 chanMap0ind = chanMap - 1; % zero-index channel
 
 % xlocation of 16 electrodes in shanks
-xlocA = [0;-22.5;0;-22.5;0;-22.5;0;-22.5;0;-22.5;0;-22.5;0;-22.5;0;-22.5];
-xlocB = xlocA + 250;
-xlocC = xlocB + 250;
-xlocD = xlocC + 250;
+xloc = [0;70;5;65;10;60;15;55;20;50;25;45;30;40;35;35];
 
-xlocAUX = [125;375;625];
-xcoords = vertcat(xlocA, xlocA, xlocA, xlocA, xlocAUX); 
+xlocAUX = ones(3,1)*35; % AUX far away
+xcoords = vertcat(xloc, xloc, xloc, xloc, xlocAUX); 
 
 % ylocation of 16 electrodes shanks
-ylocA = [0;-12.5;-25;-37.5;-50;-62.5;-75;-87.5;-100;-112.5;-125;-137.5;-150;-162.5;-175;-187.5];
-ylocB = ylocA - 300;
-ylocC = ylocB - 300;
-ylocD = ylocC - 300;
+ylocA = [0;-25;-40;-65;-80;-105;-120;-145;-160;-185;-200;-225;-240;-265;-280;-305];
+ylocB = ylocA - 600;
+ylocC = ylocB - 600;
+ylocD = ylocC - 600;
 
-% Auxiliary channels far away
-ylocAUX = [500;500;500;];
+ylocAUX = [-4600;-4650;-4700;]; % AUX far away
 ycoords = vertcat(ylocA, ylocB, ylocC, ylocD, ylocAUX);
 
-% not used in Kilosort2 (see https://github.com/MouseLand/Kilosort2/issues/155)
+% not used in Kilosort2 
+% (see https://github.com/MouseLand/Kilosort2/issues/155)
 kcoords = ones(Nchannels,1);
 
 fs = 30000;
@@ -54,6 +51,9 @@ fprintf('Creating  %s \n', fname2)
 save(fname3, ...
     'chanMap','connected','xcoords','ycoords','kcoords','chanMap0ind','fs')
 fprintf('Creating  %s \n', fname3)
+%%=======================================================================
+
+
 %%
 
 % kcoords is used to forcefully restrict templates to channels in the same
@@ -69,4 +69,3 @@ fprintf('Creating  %s \n', fname3)
 % number" of channel groups at which to set the threshold. So if a template
 % occupies more than this many channel groups, it will not be restricted to
 % a single channel group. 
-
