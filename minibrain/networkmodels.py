be6db46fb89af5c8865283e41bd16504076a8691
JoseGuzman <sjm.guzman@gmail.com>
2021-08-16 22:29:58 +0200

    starting deep Learning models

--- minibrain/networkmodels.py
+++ minibrain/networkmodels.py
@@ -5,33 +5,57 @@ Jose Guzman, jose.guzman@guzman-lab.com
 
 Created: Tue Aug 10 22:14:22 CEST 2021
 
-Contain Deep Learning models (mostly Multilayer perceptrons)
+Contain Deep Learning models
+PyTorch inherits from  nn.Module and implement some important methods:
+
+__init__: layers and activation functions will be used.
+
+forward: It specifies the computations the network needs to do when data
+is passed through it. We can use __call__ directly to invoke the forward method.
+
+predict: to quickly get the most likely label from the network.
+It calls the forward method and chooses the label with the highest score.
+
+train: used to train the network parameters.
 
 """
 
 impot torch
 from torch import nn
 
-class LRmodel(nn.Module):
+class LRNet(nn.Module):
+    """
+    A linear Neural Net with zero hidden layers is very similar to a Linear
+    regresion.
+    """
 
-    def __init__(self):
+    def __init__(self, in_dim:int, out_dim:int):
         """
-        Linear Model 
+        Linear Model
         """
-        super(LRmodel, self).__init__(in_layer, out_layer)
-        self.layers = nn.Sequential(
-            nn.Linear(), # input to hidden layer
-            nn.ReLU(),
-            nn.Linear() # output layer
+        super(LRNet, self).__init__()
+        # a linear layer has weights and bias values
+        self.out = nn.Linear(in_dim, out_dim)
 
-        )
-
-    def forward(self, x):
+    def forward(self, x:torch.Tensor):
         """
-        Forward pass
+        Forward pass to calculate prediction.
+
+        Args:
+            x (torch.Tensor): 1D tensor of features
 
         Returns:
-            torch.Tensor: model predictions
+            torch.Tensor: model prediction
         """
-        return self.layers(x)
+        y_pred = self.out(x)
+        return y_pred
 
+    def predict(self, x:torch.Tensor):
+        """
+        Model prediction
+        """
+        output = self.forward(x)
+        return output
+
+    def train(self, X, y):
+        pass
