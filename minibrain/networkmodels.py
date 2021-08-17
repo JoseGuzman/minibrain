beb1d883959ca8ff206be78b8e37db112232ca60
JoseGuzman <sjm.guzman@gmail.com>
2021-08-17 16:17:20 +0200

    update Linear model

--- minibrain/networkmodels.py
+++ minibrain/networkmodels.py
@@ -23,39 +23,49 @@ train: used to train the network parameters.
 impot torch
 from torch import nn
 
-class LRNet(nn.Module):
+class MultipleRegression(nn.Module):
     """
     A linear Neural Net with zero hidden layers is very similar to a Linear
     regresion.
     """
 
-    def __init__(self, in_dim:int, out_dim:int):
+    def __init__(self, n_weight:int):
         """
-        Linear Model
-        """
-        super(LRNet, self).__init__()
-        # a linear layer has weights and bias values
-        self.out = nn.Linear(in_dim, out_dim)
+        Initializes a multiple variable regression of the form:
 
-    def forward(self, x:torch.Tensor):
-        """
-        Forward pass to calculate prediction.
+        y = X * w + b,
+
+        where X is a matrix of observations x features, and w is a column
+        vectors of weights. b is the bias term
 
         Args:
-            x (torch.Tensor): 1D tensor of features
+            n_weight (int): the number of weights
+        """
+        super(MultipleRegression, self).__init__()
+        assert isinstance(n_weight, int)
+
+        # note that w is a column vector of size (n,1), where n
+        # is the number of independent variables.
+        self.w = torch.randn( size = (n_weight, 1), dtype = torch.float,
+            requires_grad = True)
+        self.b = torch.rand(1,
+            requires_grad = True)
+
+
+    def forward(self, X:torch.Tensor):
+        """
+        Forward pass to calculate prediction following this form:
+
+        y = X * w + b
+
+        Args:
+            X (torch.Tensor): 2D tensor of features
 
         Returns:
             torch.Tensor: model prediction
         """
-        y_pred = self.out(x)
+        y_pred = X @ self.w + self.b
         return y_pred
-
-    def predict(self, x:torch.Tensor):
-        """
-        Model prediction
-        """
-        output = self.forward(x)
-        return output
 
     def train(self, X, y):
         pass
